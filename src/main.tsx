5e58572cb4dbcfb2a2fcbc21b9406594e3aab26f
kurochkaand <kurochka.and@gmail.com>
2024-05-29 15:54:18 +0200

    Footer and styles and logo

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HeaderComponent from "./HeaderComponent";
-
-//import "./index.css";
+import FooterComponent from "./FooterComponent";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HeaderComponent />
+    <FooterComponent />
   </React.StrictMode>
 );
