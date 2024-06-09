ee2b470a7be03d6e361056eb5ba47ac8dc7543e7
kurochkaand <kurochka.and@gmail.com>
2024-06-09 20:14:29 +0200

    Set up passing of props from DragNDrop to SpectraPlotComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import HeaderComponent from "./HeaderComponent";
 import FooterComponent from "./FooterComponent";
-import TestComponent from "./TestComponent";
+import SpectraPlotComponent from "./SpectraPlotComponent";
 import DragNdrop from "./DragNdrop";
 import { useEffect, useState } from "react";
 
 function App() {
   const [files, setFiles] = useState<File[]>([]);
-  useEffect(() => {
-    console.table(files);
-  }, [files]);
+
   return (
     <>
       <HeaderComponent />
       <DragNdrop height={200} width={200} onFilesSelected={setFiles} />
-      <TestComponent />
+      <SpectraPlotComponent files={files} />
       <FooterComponent />
     </>
   );
---
+++ src/SpectraPlotComponent.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { xyToXYObject } from "ml-spectra-processing";
+import { Annotations, Axis, LineSeries, Plot, PlotController, SeriesPoint, useRectangularZoom } from "react-plot";
+import { convert as convertJcamp } from "jcampconverter";
+
+interface InfraredZoomablePlotProps {
+  files: File[] | undefined;
+}
+
+export default function InfraredZoomablePlot(props: InfraredZoomablePlotProps) {
+  return (
+    <PlotController>
+      <ZoomablePlot files={props.files} />
+    </PlotController>
+  );
+}
+
+interface ZoomablePlotProps {
+  files: File[] | undefined;
+}
+
+function ZoomablePlot(props: ZoomablePlotProps) {
+  const [data, setData] = useState<SeriesPoint[]>([]);
+  const zoom = useRectangularZoom();
+
+  // useEffect(() => {
+  //   if (files.length > 0) {
+  //     const reader = new FileReader();
+  //     reader.onload = (event: ProgressEvent<FileReader>) => {
+  //       if (event.target && typeof event.target.result === "string") {
+  //         console.log(event.target.result);
+  //       }
+  //     };
+  //     reader.readAsText(files[0]);
+  //   }
+  // }, [files]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (props.files && props.files.length > 0) {
+        const response = props.files[0];
+        const jcamp = await response.text();
+        const jcampData = convertJcamp(jcamp).flatten[0].spectra[0].data;
+        const xyData = xyToXYObject(jcampData).map((point: { x: number; y: number }) => ({
+          x: point.x,
+          y: point.y * 100,
+        }));
+        setData(xyData);
+      } else {
+        setData([{ x: 0, y: 0 }]);
+      }
+    };
+    fetchData();
+  }, [props.files]);
+
+  return (
+    <Plot width={800} height={300}>
+      <LineSeries data={data} xAxis="x" yAxis="y" />
+      <Axis id="x" position="bottom" label="Wavenumber (cm-1)" displayPrimaryGridLines flip={true} />
+      <Axis id="y" position="left" label="Transmitance (%)" displayPrimaryGridLines />
+      <Annotations>{zoom.annotations}</Annotations>
+    </Plot>
+  );
+}
